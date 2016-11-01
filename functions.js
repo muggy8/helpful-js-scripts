d33c60861127014e338ccab73e30e28872cdd707
Muggy Ate <muggy8@gmail.com>
2016-10-31 18:42:05 -0700

    mre functions yay

--- functions.js
+++ functions.js
@@ -14,3 +14,16 @@ function toggleClass(el, toggleClassName, animEndCallbackOnce){
 		el.addEventListener("transitionend", callBackOnce);
 	}
 }
+
+function ajaxGet(url, successCallback, failCallback){
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	
+	successCallback = successCallback || function (data){/*console.log(data)*/};
+	failCallback = failCallback || function (data){/*console.log(data)*/};
+	
+	request.onload = function(){successCallback(request)};
+	request.onerror = function(){failCallback(request)};
+	
+	request.send();
+}
